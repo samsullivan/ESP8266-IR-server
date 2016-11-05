94b24f3411838a4647c1dc2c60918dc4ad4e2df3
Sam Sullivan <samsullivan09@gmail.com>
2016-11-05 15:40:38 -0500

    Return JSON responses from server.

--- main.ino
+++ main.ino
@@ -1,11 +1,40 @@
 #include "WiFi.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
 
+StaticJsonBuffer<200> jsonBuffer;
 ESP8266WebServer server(80);
 
+void sendSuccessfulResponse() {
+  JsonObject& response = jsonBuffer.createObject();
+  response["success"] = true;
+
+  char buffer[256];
+  response.printTo(buffer, sizeof(buffer));
+
+  server.send(200, "application/json", buffer);
+}
+
+void sendFailedResponse(char* message = "", int code = 500) {
+  JsonObject& response = jsonBuffer.createObject();
+  response["success"] = false;
+  if(sizeof(message)) {
+    response["error"] = message;
+  }
+
+  char buffer[256];
+  response.printTo(buffer, sizeof(buffer));
+
+  server.send(code, "application/json", buffer);
+}
+
 void handleRoot() {
-  server.send(200, "text/html", "Hello World!");
+  sendSuccessfulResponse();
+}
+
+void handle404() {
+  sendFailedResponse("Page not found.", 404);
 }
 
 void setup() {
@@ -27,6 +56,7 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   server.on("/", handleRoot);
+  server.onNotFound(handle404);
   server.begin();
 }
 
